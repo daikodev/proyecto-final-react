34d0ca1582afbfd6ba49de80ffed56ba0e356791
DaikoDev <cdanieleon28@gmail.com>
2024-07-01 11:49:21 -0500

    Cambio de app.css a la carpeta styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './views/Login';
 import Layout from './components/layout/Layout';
