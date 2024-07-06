c62bf72539706b8ec5544ee369c168fb3a2091cd
DaikoDev <cdanieleon28@gmail.com>
2024-07-05 20:36:18 -0500

    Diseño para tarjeta de productos

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,6 +3,14 @@ import StarRatings from 'react-star-ratings'
 import '../styles/ProductCard.css'
 
 function ProductCard({ product }) {
+
+    const formatCurrency = (price) => new Intl.NumberFormat('es-ES', {
+        style: 'currency',
+        currency: 'PEN',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    }).format(price);
+
     return (
         <>
             <div className="col-3">
@@ -10,15 +18,18 @@ function ProductCard({ product }) {
                     <img src={product.image} alt="{product.title}" className="card-img-top img-fluid p-3" />
                     <div className="card-body">
                         <h6 className="text-secondary">{product.category.toUpperCase()}</h6>
-                        <h5 className="card-title">{product.title}</h5>
-                        <div className='d-flex align-items-center justify-content-between'>
+                        <h4 className="card-title m-0">{product.title}</h4>
+                        <p className="card-text m-0">{product.description}</p>
+
+                        <div className='d-flex align-items-center justify-content-between my-2'>
                             <StarRatings rating={product.rating.rate} starRatedColor='gold' numberOfStars={5} starDimension='20px' starSpacing='2px' name='rating' className='d-flex align-items-center' />
                             <h6 className='m-0'>{product.rating.count} votos</h6>
                         </div>
-                        <div className='description'>
-                            <p className="card-text">{product.description}</p>
+
+                        <div className='d-flex align-items-center w-100 justify-content-between mt-3'>
+                            <h5 className='m-0'>{formatCurrency(product.price)}</h5>
+                            <button className='btn btn-dark fs-6'>Agregar al carrito</button>
                         </div>
-                        <span>{product.price}</span>
                     </div>
                 </div>
             </div>
