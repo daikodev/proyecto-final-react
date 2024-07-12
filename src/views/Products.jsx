7a4cbc0990a98134ae7d9ad96528650b5da7f122
DaikoDev <cdanieleon28@gmail.com>
2024-07-12 01:05:53 -0500

    style: Agregar boton para registrar un nuevo producto

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -1,46 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import ProductRow from '../components/ProductRow';
+import React, { useEffect, useState } from "react";
+import ProductRow from "../components/ProductRow";
 
 function Products() {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    const handleGetProduct = async () => {
-        fetch("https://fakestoreapi.com/products")
-            .then(res => res.json())
-            .then(data => {
-                setProducts(data);
-            })
-    }
+  const handleGetProduct = async () => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  };
 
-    useEffect(() => {
-        handleGetProduct();
-    }, [])
+  useEffect(() => {
+    handleGetProduct();
+  }, []);
 
-    return (
-        <>
-            <section className="container py-5">
-                <div className="table-responsive">
-                    <table className="table table-bordered table-hover">
-                        <thead>
-                            <tr>
-                                <th scope="col">ID</th>
-                                <th scope="col">CATEGORÍA</th>
-                                <th scope="col">TÍTULO</th>
-                                <th scope="col">DESCRIPCIÓN</th>
-                                <th scope="col">PRECIO</th>
-                                <th scope="col">IMAGEN</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {products.map((product) => (
-                                <ProductRow key={product.id} product={product} />
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className="container py-3">
+        <div className="d-flex align-items-center justify-content-end mb-2">
+          <button className="btn btn-dark">+ Agregar Producto</button>
+        </div>
+
+        <div className="table-responsive">
+          <table className="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">CATEGORÍA</th>
+                <th scope="col">TÍTULO</th>
+                <th scope="col">DESCRIPCIÓN</th>
+                <th scope="col">PRECIO</th>
+                <th scope="col">IMAGEN</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <ProductRow key={product.id} product={product} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Products
+export default Products;
